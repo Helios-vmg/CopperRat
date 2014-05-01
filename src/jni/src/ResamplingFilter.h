1648cc0eb66b5c03110b59333bf33ed66c4946d4
Helios-vmg <helios.vmg@gmail.com>
2014-05-01 17:54:47 -0300

    Fixed bug in resampling class.

--- src/jni/src/ResamplingFilter.h
+++ src/jni/src/ResamplingFilter.h
@@ -8,10 +8,16 @@ protected:
 	unsigned src_rate;
 	unsigned dst_rate;
 public:
-	ResamplingFilter(Decoder &decoder, unsigned dst_rate): decoder(decoder), dst_rate(dst_rate){}
+	ResamplingFilter(Decoder &decoder, unsigned dst_rate): decoder(decoder), src_rate(decoder.get_sampling_rate()), dst_rate(dst_rate){}
 	virtual ~ResamplingFilter(){}
-	virtual sample_count_t read(audio_buffer_t buffer, audio_position_t position);
+	virtual sample_count_t read(audio_buffer_t buffer, audio_position_t position) = 0;
 	static ResamplingFilter *create(Decoder &decoder, unsigned dst_rate);
+};
+
+class IdentityResamplingFilter : public ResamplingFilter{
+public:
+	IdentityResamplingFilter(Decoder &decoder, unsigned dst_rate): ResamplingFilter(decoder, dst_rate){}
+	sample_count_t read(audio_buffer_t buffer, audio_position_t position);
 };
 
 class UpsamplingFilter : public ResamplingFilter{
