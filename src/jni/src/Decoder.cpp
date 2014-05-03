e9b87c631208a896693c3468de604df89015cd26
Helios-vmg <helios.vmg@gmail.com>
2014-05-03 01:44:22 -0300

    Fixed bug in Decoder::read_more().

--- src/jni/src/Decoder.cpp
+++ src/jni/src/Decoder.cpp
@@ -6,7 +6,12 @@ Decoder *Decoder::create(const char *filename){
 }
 
 audio_buffer_t Decoder::read_more(audio_position_t initial_position){
+	audio_buffer_t ret;
 	if (this->current_position != initial_position && !this->seek(initial_position))
-		return weak_audio_buffer_t();
-	return this->read_more();
+		return ret;
+	ret = this->read_more();
+	if (!ret)
+		return ret;
+	this->current_position += ret.samples();
+	return ret;
 }
