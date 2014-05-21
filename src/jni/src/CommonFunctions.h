26d948e5a59b55e9788b2ddb3ea2d21c854af0dd
Helios-vmg <helios.vmg@gmail.com>
2014-05-21 04:13:28 -0300

    Optimized the shit out of the UTF-8 functions.

--- src/jni/src/CommonFunctions.h
+++ src/jni/src/CommonFunctions.h
@@ -2,6 +2,8 @@
 #define COMMONFUNCTIONS_H
 #include "BasicTypes.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 inline double s16_to_double(Sint16 x){
 	return (x<0) ? (x/32768.0) : (x/32767.0);
@@ -50,56 +52,105 @@ struct Unpointify<T *>{
 
 void parse_into_hms(std::ostream &stream, double total_seconds);
 
-
 template <typename T>
-bool utf8_to_string(std::basic_string<T> &dst, unsigned char *buffer, size_t n){
+void utf8_to_string(std::basic_string<T> &dst, const unsigned char *buffer, size_t n){
+	static const unsigned char utf8_lengths[] = {
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+		3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+		4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 1, 1
+	};
 	static const unsigned char bom[] = { 0xEF, 0xBB, 0xBF };
+	static const unsigned char masks[] = { 0x00, 0xFF, 0x1F, 0x0F, 0x07, 0x03, 0x01 };
 	if (n >= 3 && !memcmp(buffer, bom, 3)){
 		buffer += 3;
 		n -= 3;
 	}
-	size_t utf8_size = 0;
-	for (size_t i = 0; i < n; i++)
-		if (buffer[i] < 128 || (buffer[i] & 192) == 192)
-			utf8_size++;
-	std::basic_string<T> &ret = dst;
-	ret.reserve(utf8_size);
-	for (size_t i = 0; i != n;){
-		unsigned char byte = buffer[i++];
-		T wc = 0;
-		if (!(byte & 0x80))
-			wc = (T)byte;
-		else if ((byte & 0xC0) == 0x80)
-			return 0;
-		else if ((byte & 0xE0) == 0xC0){
-			if (n - i < 2)
-				return 0;
-			wc = (T)(byte & 0x1F);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-		}else if ((byte & 0xF0) == 0xE0){
-			if (n - i < 3)
-				return 0;
-			wc = (T)(byte & 0x0F);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-		}else if ((byte & 0xF8) == 0xF0){
-			if (n - i < 4)
-				return 0;
-			wc = (T)(byte & 0x07);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-			wc <<= 6;
-			wc |= (T)(buffer[i++] & 0x3F);
-		}else
-			return 0;
-		ret.push_back(wc);
+	size_t truncated_length = 0,
+		size = 0;
+	for (truncated_length = 0; truncated_length < n;){
+		auto l = utf8_lengths[buffer[truncated_length]];
+		if (truncated_length + l > n)
+			break;
+		size++;
+		truncated_length += l;
 	}
-	return 1;
+	dst.resize(size);
+	size_t writer = 0;
+	T *pointer = &dst[0];
+	for (size_t i = 0; i != truncated_length;){
+		unsigned char byte = buffer[i++];
+		if (i == 7563)
+			byte = byte;
+		auto length = utf8_lengths[byte];
+		unsigned wc = byte & masks[length];
+		while (--length){
+			wc <<= 6;
+			wc |= (T)(buffer[i++] & 0x3F);
+		}
+		pointer[writer++] = wc;
+	}
+}
+
+inline unsigned utf8_bytes(unsigned c){
+	if (c < 0x00000080)
+		return 1;
+	if (c < 0x00000800)
+		return 2;
+	if (c < 0x00010000)
+		return 3;
+	if (c < 0x00200000)
+		return 4;
+	if (c < 0x04000000)
+		return 5;
+	return 6;
+}
+
+template <typename T>
+size_t utf8_size(const std::basic_string<T> &s){
+	size_t ret = 0;
+	for (auto c : s)
+		ret += utf8_bytes(c);
+	return ret;
+}
+
+template <typename T>
+void string_to_utf8(std::vector<unsigned char> &dst, const std::basic_string<T> &src){
+	static const unsigned char masks[] = { 0, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC };
+	dst.resize(utf8_size(src));
+	unsigned char *pointer = &dst[0];
+	const T *src_pointer = &src[0];
+	size_t writer = 0;
+	for (size_t i = 0, n = src.size(); i != n; i++){
+		unsigned c = src_pointer[i];
+
+		if (!(c & 0x80)){
+			*(pointer++) = (unsigned char)c;
+			continue;
+		}
+
+		unsigned char temp[6];
+		unsigned temp_size = 0;
+		do{
+			temp[temp_size++] = c & 0x3F | 0x80;
+			c >>= 6;
+		}while (c & ~0x3F);
+		*(pointer++) = c | masks[temp_size];
+		while (temp_size)
+			*(pointer++) = temp[--temp_size];
+	}
 }
 
 template <typename T1, typename T2>
