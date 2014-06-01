e91dcb37f58f91fa56cba19690d3103cc26d3799
Helios-vmg <helios.vmg@gmail.com>
2014-06-01 10:55:23 -0300

    Implemented playlist.

--- src/jni/src/Playlist.h
+++ src/jni/src/Playlist.h
@@ -1,6 +1,34 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
 #include "Metadata.h"
 
 class PlayItem{
 	std::string path;
 
 };
+
+class Playlist{
+public:
+	enum class PlaybackMode{
+		SINGLE = 0,
+		REPEAT_LIST = 1,
+		REPEAT_TRACK = 2,
+		COUNT = 3,
+	};
+private:
+	size_t current_track;
+	bool current_track_is_repeated;
+	PlaybackMode mode;
+	bool shuffle;
+	std::vector<std::wstring> tracks;
+	std::vector<size_t> shuffle_vector;
+public:
+	Playlist(): current_track(0), mode(PlaybackMode::REPEAT_LIST), shuffle(0), current_track_is_repeated(0){}
+	void insert(const std::vector<std::wstring> &, size_t);
+	void toggle_shuffle();
+	bool pop(std::wstring &);
+	PlaybackMode cycle_mode();
+};
+
+#endif
